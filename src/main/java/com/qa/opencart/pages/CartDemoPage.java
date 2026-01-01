66f7cc844e1b6e11d310a091e48e275ee1729a89
TanyaSaha <ravindra.kamisetty@gmail.com>
2026-01-02 02:46:19 +0530

    Added Cart Demo Page

---
+++ src/main/java/com/qa/opencart/pages/CartDemoPage.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class CartDemoPage {
+
+	public static void main(String[] args) {
+		System.out.println("This is demo class --- Cart Demo Page");
+
+	}
+
+}
