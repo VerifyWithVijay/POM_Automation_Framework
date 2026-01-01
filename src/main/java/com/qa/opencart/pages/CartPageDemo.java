f6cc9411d8fe89cfedcdc0e51f1ae3bab310fe63
TanyaSaha <ravindra.kamisetty@gmail.com>
2026-01-02 02:36:24 +0530

    Added Cart Page

---
+++ src/main/java/com/qa/opencart/pages/CartPageDemo.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CartPageDemo {
+	private static final Logger log = LogManager.getLogger(LoginPage.class);
+	
+	private static int cartCount=10;
+	
+	public void verifyCartFeature()
+	{
+		log.info("Validate Cart Feature");
+	}
+
+}
